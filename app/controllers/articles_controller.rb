f34139a6845ecc46826a564091203d5ce68e27c1
kurt weber <kurt@zipnosis.com>
2015-10-11 20:09:52 -0500

    added categories to articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,12 @@ class ArticlesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
-    @articles = Article.all.order("created_at DESC")
+    if params[:category].blank?
+      @articles = Article.all.order("created_at DESC")
+    else
+      @category_id = Category.find_by(name: params[:category]).id
+      @articles = Article.where(category_id: @category_id).order("created_at DESC")
+    end
   end
 
   def show
@@ -23,12 +28,12 @@ class ArticlesController < ApplicationController
   end
 
   private
-
-  def article_params
-    params.require(:article).permit(:title, :content)
-  end
-
   def find_article
     @article = Article.find(params[:id])
   end
+
+  def article_params
+    params.require(:article).permit(:title, :content, :category_id)
+  end
+
 end
